6a3c1d24855a948eb164e7632aa4a85538966575
李吉祥01 <jixiang01.li@vipshop.com>
2022-05-29 13:29:28 +0800

    修正信息

--- src/main/java/com/atguigu/bean/WaterSensor.java
+++ src/main/java/com/atguigu/bean/WaterSensor.java
@@ -16,6 +16,39 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class WaterSensor {
     private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getTs() {
+        return ts;
+    }
+
+    public void setTs(Long ts) {
+        this.ts = ts;
+    }
+
+    public Integer getVc() {
+        return vc;
+    }
+
+    public void setVc(Integer vc) {
+        this.vc = vc;
+    }
+
     private Long ts;
     private Integer vc;
+
+    public WaterSensor(String id,Long ts,Integer vc)
+    {
+        this.id = id;
+        this.ts = ts;
+        this.vc = vc;
+
+    };
 }
